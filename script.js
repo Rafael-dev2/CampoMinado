ae571afb00c08f476728232e85e02849e92d8fa7
Rafael-dev2 <rafasantosalves2013@gmail.com>
2024-08-17 12:13:45 -0300

    Melhorias no código

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 canvas = document.querySelector("#Sketch");
 createbtn = document.querySelector("#topbtn");
 deletebtn = document.querySelector("#deletebtn");
+let redPixel = "background-color: red;display: inline-flex;align-self: center;border: 1px solid gray;box-sizing: border-box;";
+let whitePixel = "background-color: white;display: inline-flex;align-self: center;border: 1px solid gray;box-sizing: border-box;";
 let input;
 createbtn.addEventListener("click",() => {
     input = prompt("How many squares?");
@@ -12,38 +14,36 @@ createbtn.addEventListener("click",() => {
 deletebtn.addEventListener("click",() =>{
     document.querySelector("#subcanvas").remove();
 })
-
+function paintPixel(posx,posy){
+    finder = 'div[posy="'+posy+'"]';
+    let targetLine = document.querySelector(finder);
+    finder = 'div[posx="'+posx+'"]';
+    let pixel = targetLine.querySelector(finder);
+    pixel.setAttribute("style",redPixel+pixelSize);
+}
 function generateCanvas(input){
 subcanvas = document.createElement("div");
 subcanvas.setAttribute("id","subcanvas");
 
-size = 960/input;
+size = 720/input;
 str = size.toString();
+pixelSize = "width:"+str+"px;"+"height:"+str+"px;";
 for(i = 0;i < input;i++){
     let line = document.createElement("div");
     line.setAttribute("id","line")
     line.setAttribute("style","font-size: 0px;")
+    line.setAttribute("posy",i.toString());
     for(j = 0;j < input;j++){
         let pixel = document.createElement("div");
-        pixel.setAttribute("style","background-color: white;" + "width:"+str+"px;"+
-            "height: "+str+"px;"+ 
-            'display: inline-flex;'+
-            'align-self: center;'+
-            'border: 1px solid gray;'+
-            'box-sizing: border-box;'
-                     );
+        pixel.setAttribute("style",whitePixel+pixelSize);
         pixel.setAttribute("id","pixel");
+        pixel.setAttribute("posx",j.toString());
+        pixel.addEventListener("click",() => {
+            positionX = parseInt(pixel.getAttribute("posx"));
+            positionY = parseInt(pixel.parentElement.getAttribute("posy"));
+            paintPixel(positionX,positionY);
+        })
         line.appendChild(pixel);
-        pixel.addEventListener("mouseover",() => {
-        pixel.setAttribute("style","background-color: red;"+"width:"+str+"px;"+
-            "height: "+str+"px;"+
-            'display: inline-flex;'+
-            'align-self: center;'+ 
-            'text-color:red;'+
-            'border: 1px solid gray;'+
-            'box-sizing: border-box;'
-     );
-    })
 }
     subcanvas.appendChild(line);
     canvas.appendChild(subcanvas);
