98d1c2aa050d41cd71bfb240b3ba0f415c87950f
Rafael-dev2 <rafasantosalves2013@gmail.com>
2024-08-22 06:33:23 -0300

    Versão 0.5 - Adição de score

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 canvas = document.querySelector("#Sketch");
 createbtn = document.querySelector("#topbtn");
 deletebtn = document.querySelector("#deletebtn");
-let redPixel =   "background-color: red  ;display: inline-flex;align-self: center;border: 1px solid gray;box-sizing: border-box;";
-let whitePixel = "background-color: white;display: inline-flex;align-self: center;border: 1px solid gray;box-sizing: border-box;";
-let greenPixel = "background-color: green;display: inline-flex;align-self: center;border: 1px solid gray;box-sizing: border-box;";
-let bluePixel = "background-color: blue;display: inline-flex;align-self: center;border: 1px solid gray;box-sizing: border-box;";
+const redPixel =   "font-weight: bold;color: white;background-color: red  ;display: inline-flex;align-self: center;border: 1px solid gray;box-sizing: border-box;";
+const whitePixel = "font-weight: bold;background-color: white;display: inline-flex;align-self: center;border: 1px solid gray;box-sizing: border-box;";
+const greenPixel = "font-weight: bold;color: white;background-color: green;display: inline-flex;align-self: center;border: 1px solid gray;box-sizing: border-box;";
+const bluePixel = "font-weight: bold;color: white;background-color: blue;display: inline-flex;align-self: center;border: 1px solid gray;box-sizing: border-box;";
 let lineStyle = "font-size: 16px;";
 let input;
 let ratioMines;
 let numberMines = 0;
+let textNumberMines = 0;
 let hasCanvas = false;
+let gameState = "start";
+let gameOver = false;
 createbtn.addEventListener("click",() => {
+    if (gameState === "playing" || gameState === "win" || gameState === "lose") {
+        numberMines = 0
+        textNumberMines = 0;
+        document.querySelector("#score").remove();
+    }
     input = prompt("Digite o tamanho do tabuleiro");
     if(input > 100){
         input = 100;
@@ -30,25 +38,14 @@ deletebtn.addEventListener("click",() =>{
     hasCanvas = false;
 })
 
-function locatePixel(posx,posy){
-    if(posx >= 0 && posy >= 0 && posx < input && posy < input){
-        finder = 'div[posy="'+posy+'"]';
-        let targetLine = document.querySelector(finder);
-        finder = 'div[posx="'+posx+'"]';
-        return targetLine.querySelector(finder);
-    }
-    return -1;
-}
-function paintPixel(posx,posy,color){
-    pixel = locatePixel(posx,posy);
-    pixel.setAttribute("style",color+pixelSize);
-}
 function generateCanvas(input){
 subcanvas = document.createElement("div");
 subcanvas.setAttribute("id","subcanvas");
 size = 80/input;
 str = size.toString();
+lineStyle = "font-size: "+size/2+"vh;"
 pixelSize = "width:"+str+"vh;"+"height:"+str+"vh;";
+gameState = "playing";
 for(i = 0;i < input;i++){
     let line = document.createElement("div");
     line.setAttribute("id","line")
@@ -59,6 +56,7 @@ for(i = 0;i < input;i++){
         pixel.setAttribute("style",whitePixel+pixelSize);
         pixel.setAttribute("id","pixel");
         if(Math.floor(Math.random() * 100) <= ratioMines){
+            console.log("teste"+i+j+"\n");
             pixel.setAttribute("hasMine","true");
             numberMines += 1;
         }
@@ -68,34 +66,55 @@ for(i = 0;i < input;i++){
             positionX = parseInt(pixel.getAttribute("posx"));
             positionY = parseInt(pixel.parentElement.getAttribute("posy"));
             if(pixel.getAttribute("hasMine") === "true"){
-                //alert("Você Explodiu!");
-                paintPixel(positionX,positionY,greenPixel);
-            }else{paintPixel(positionX,positionY,redPixel);}
+                if(gameOver === false){
+                    alert("Você Explodiu!");
+                    gameOver = true;
+                    gameState = "lose";
+                    revealMap();
+                }
+                paintPixel(positionX,positionY,redPixel);
+                pixel.textContent = "!";
+            }else{paintPixel(positionX,positionY,bluePixel);
+            }
             sum = countMines(positionX,positionY);
-            pixel.textContent = sum.toString();
+            if(pixel.getAttribute("hasMine") !== "true") {
+                pixel.textContent = sum.toString();
+            }
             if(sum === 0){
                 recursiveClick(positionX,positionY);
             }
             pixel.removeEventListener("click",playerClick);
         })
-        pixel.addEventListener("contextmenu",function rightClick(){
+        pixel.addEventListener("contextmenu",function rightClick(ev){
+            ev.preventDefault();
             positionX = parseInt(pixel.getAttribute("posx"));
             positionY = parseInt(pixel.parentElement.getAttribute("posy"));
             if(pixel.getAttribute("hasMine") === "true"){
                 numberMines -= 1;
-                paintPixel(positionX,positionY,bluePixel);
+            }
+                textNumberMines -= 1;
+                paintPixel(positionX,positionY,greenPixel);
                 pixel.textContent = "V";
                 if(numberMines === 0){
                     alert("Você Venceu!");
+                    gameState = "win";
+                    revealMap();
                 }
+                score = document.querySelector("#score");
+                score.remove();
+                generateScore();
                 pixel.removeEventListener("click",rightClick);
-            }
+
+
         })
         line.appendChild(pixel);
 }
     subcanvas.appendChild(line);
     canvas.appendChild(subcanvas);
+
 }
+    textNumberMines = numberMines;
+    generateScore();
 }
 function countMines(posx,posy){
     let sum = 0;
@@ -135,6 +154,41 @@ function recursiveClick(posx,posy){
             if(pixel !== -1 && pixel.getAttribute("hasMine") !== "true"){
                 pixel.click();
             }
+        }
+    }
+}
+function generateScore(){
+    sidebar = document.querySelector("#Sidebar");
+    score = document.createElement("div");
+    score.setAttribute("id","score");
+    score.textContent = "Minas Restantes:"+textNumberMines;
+    sidebar.appendChild(score);
+}
+function locatePixel(posx,posy){
+    if(posx >= 0 && posy >= 0 && posx < input && posy < input){
+        finder = 'div[posy="'+posy+'"]';
+        let targetLine = document.querySelector(finder);
+        finder = 'div[posx="'+posx+'"]';
+        return targetLine.querySelector(finder);
+    }
+    return -1;
+}
+function paintPixel(posx,posy,color){
+    pixel = locatePixel(posx,posy);
+    pixel.setAttribute("style",color+pixelSize);
+}
+function revealMap(){
+    for(let i = 0; i < input; i++){
+        for(let j = 0; j < input; j++){
+            pixel = locatePixel(i,j);
+            if(gameState == "lose"){
+                pixel.click();
+            }else if(gameState == "win"){
+                if(pixel.getAttribute("hasMine") !== "true"){
+                    pixel.click();
+                }
+            }
+
         }
     }
 }
